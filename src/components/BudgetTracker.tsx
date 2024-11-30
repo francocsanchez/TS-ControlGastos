3d955940dc9b417fd8641a282f94cb7d6c4ec36f
Franco Sanchez <francosanchez@Francos-MacBook-Pro.local>
2024-11-30 09:13:51 -0300

    add graph circular

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,13 +1,30 @@
+import { useMemo } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function BudgetTracker() {
   const { state, dispatch } = useBudget();
+
+  const totalExpenses = useMemo(() => state.expenses.reduce((total, expense) => total + expense.amount, 0), [state.expenses]);
+  const enableExpenses = state.budget - totalExpenses;
+
+  const percentage = +((totalExpenses / state.budget) * 100).toFixed(2);
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         <div className="flex justify-center">
-          <img src="/grafico.jpg" alt="grafico de gastos" />
+          <CircularProgressbar
+            value={percentage}
+            styles={buildStyles({
+              pathColor: percentage >= 100 ? "#DC2626" : "#3B82F6",
+              trailColor: "#F5F5F5",
+              textSize: 15,
+              textColor: percentage >= 100 ? "#DC2626" : "#3B82F6",
+            })}
+            text={`${percentage}%`}
+          />
         </div>
         <div className="flex flex-col justify-center items-start gap-8">
           <button
@@ -18,8 +35,8 @@ export default function BudgetTracker() {
             Resetear App
           </button>
           <AmountDisplay label="Presupuesto" amount={state.budget} />
-          <AmountDisplay label="Disponible" amount={200} />
-          <AmountDisplay label="Gastado" amount={100} />
+          <AmountDisplay label="Disponible" amount={enableExpenses} />
+          <AmountDisplay label="Gastado" amount={totalExpenses} />
         </div>
       </div>
     </>
